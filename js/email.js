7cb6e381e583917f4271e72a9ca0bd56eb990028
tcmbonani <tcmbonani@gmail.com>
2024-02-02 07:28:57 +0200

    email

--- js/email.js
+++ js/email.js
@@ -1,21 +1,18 @@
-// Get form values
-var email = document.getElementById("email").value;
-var subject = document.getElementById("subject").value;
-var message = document.getElementById("message").value;
 
-// Make a POST request with Fetch API
-fetch("https://formsubmit.co/ajax/tcmbonani@gmail.com", {
+// https://github.com/github/fetch
+fetch("https://formsubmit.co/f00f82cc40d840aad602d4354cd354fb", {
     method: "POST",
     headers: { 
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     },
     body: JSON.stringify({
-        email: email,
-        subject: subject,
-        message: message
+        email: "FormSubmit",
+        sunject: "",
+        message: "I'm from Devro LABS"
     })
 })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.log(error));
+
